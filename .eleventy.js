9184644dd569567875ddc5e0847d665b66513e71
IXI B <ixi.b@tuta.io>
2026-08-08 05:49:01 -0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,16 @@
 module.exports = function(eleventyConfig) {
+  // CMS
   eleventyConfig.addPassthroughCopy("admin");
+
+  // Archivos estáticos que siguen estando en la raíz
+  eleventyConfig.addPassthroughCopy("*.html");
+  eleventyConfig.addPassthroughCopy("*.css");
+  eleventyConfig.addPassthroughCopy("*.js");
+  eleventyConfig.addPassthroughCopy("*.jpg");
+  eleventyConfig.addPassthroughCopy("*.png");
+  eleventyConfig.addPassthroughCopy("*.xml");
+  eleventyConfig.addPassthroughCopy("*.txt");
+
   return {
     dir: {
       input: "src",
