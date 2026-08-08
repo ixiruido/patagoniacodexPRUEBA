ccc8032d91d1c13c44fc59f2450933bd39312461
IXI B <ixi.b@tuta.io>
2026-08-08 07:49:03 -0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,19 +2,8 @@ module.exports = function(eleventyConfig) {
   // CMS
   eleventyConfig.addPassthroughCopy("admin");
 
-  // Archivos estáticos de src/
-  eleventyConfig.addPassthroughCopy("src/*.css");
-  eleventyConfig.addPassthroughCopy("src/*.js");
-  eleventyConfig.addPassthroughCopy("src/assets/images");
-
-  // Archivos estáticos que siguen estando en la raíz (para HTML originales)
-  eleventyConfig.addPassthroughCopy("*.html");
-  eleventyConfig.addPassthroughCopy("*.css");
-  eleventyConfig.addPassthroughCopy("*.js");
-  eleventyConfig.addPassthroughCopy("*.jpg");
-  eleventyConfig.addPassthroughCopy("*.png");
-  eleventyConfig.addPassthroughCopy("*.xml");
-  eleventyConfig.addPassthroughCopy("*.txt");
+  // Imágenes y otros archivos estáticos dentro de src/
+  eleventyConfig.addPassthroughCopy("src/assets");
 
   return {
     dir: {
